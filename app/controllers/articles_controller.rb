6a85660e34c7dcef0553ecb0f9508e5c4770f8d5
Natali <rinykia@gmail.com>
2015-10-01 16:05:22 +0300

    Fix bug

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
-  before_filter :authenticate_user, only => [:new, :create]
+  before_filter :authenticate_user!
+
   def index
     @articles = Article.all
   end
