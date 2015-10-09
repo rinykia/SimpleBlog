c3554b7457daf2ae3ce15abfa127f2a6bfef88d1
Natali <rinykia@gmail.com>
2015-10-09 14:50:55 +0300

    Update contacts new view

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 feature "Contact Creation" do
   scenario "allows acces to contacts page" do
     visit '/contacts'
-    
-    expect(page).to have_content 'Contact us'
+
+    expect(page).to have_content 'Contact Us'
   end
 end
